33d9027156fc03d14cc7f21c55c54dce7f7809fa
n0step_ <krushnavyas06@gmail.com>
2025-03-19 18:16:53 +0000

    fix: no need for headers (was for debugging only)

--- src/services/matchHistoryService.js
+++ src/services/matchHistoryService.js
@@ -14,10 +14,7 @@ async function getMatchHistory(teamId) {
     try {
       const html = await request({
         uri: url,
-        transform: (body) => cheerio.load(body),
-        headers: {
-          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-        }
+        transform: (body) => cheerio.load(body)
       });
 
       const $ = html;
